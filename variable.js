026aa19398651c3dad5ad0340179ef5bd6b1b973
sparkmahadi <mahadi.developer@gmail.comm>
2022-07-31 21:28:06 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,32 @@
+//number type variable
+var sunglassPrice = 120;
+
+//string type variable
+var specialOneName = 'Ariana Grande';
+
+// boolean type variable
+var isSingle = true;
+
+// two types of number: Integer: 120,
+// float, decimal: 3.65,
+
+//variable naming convention: rules, best practice
+/*
+1. singel word
+2. no gap
+3. no quote
+4. no keyword
+5. cannot start withe a number
+6. can not contain dash
+7. should use camelCase
+*/
+
+// Math operations: +, -, *, /
+// Shorthand: +=, -=, ++, --
+
+// convert: parseInt, parseFloat
+
+// toFixed
+
+// remainder : vagsesh: % or modulus
+
